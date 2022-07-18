a0a51085126ce3bb689e6e3cfe63b8191e110cdf
bisheshwarpandey6642 <bisheshwarpandey6642>
2022-07-18 21:55:45 +0530

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyparser = require("body-parser")
 const mongoose =require('mongoose')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 5000
 
 
 
