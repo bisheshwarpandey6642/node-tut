c3aaa53590ea5ec5b13ae627b5672f3fc6066f5d
bisheshwarpandey6642 <61419944+bisheshwarpandey6642@users.noreply.github.com>
2023-07-13 12:43:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const postRoutes = require("./router/routes")
 const mongoose =require('mongoose')
 const app = express()
 app.use('/display', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+
+app.use(bodyparser.json());
+app.use('/api', postRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        message:"running"
+    })
+})
+
 app.listen(process.env.PORT || 3000)
